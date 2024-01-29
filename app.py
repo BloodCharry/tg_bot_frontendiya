8400fb4c51cb9637750ed7137c5554da3a698fcc
BloodCharry <vladvorobei1994@gmail.com>
2024-01-29 20:35:47 +0300

    FIRST START

---
+++ app.py
@@ -0,0 +1,36 @@
+import asyncio
+
+from aiogram import (
+    Bot,
+    Dispatcher,
+    types
+)
+
+from aiogram.filters import CommandStart
+
+bot = Bot(token="5801532244:AAEZeAKdLYMsO7bGZkLFrL1XgOg57f3hRNg")
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def on_startup(message: types.Message):
+    await message.answer("Это была команда старт!")
+
+
+@dp.message()
+async def echo(message: types.Message):
+    text = message.text
+
+    if text in ["Привет", "привет", "hi", "Hi", "хай", "Хай", "hello", "Hello"]:
+        await message.answer("И тебе привет!")
+    elif text in ["Пока", "пока", "bye", "Bye", "goodbye", "Goodbye", "до свидания", "До свидания"]:
+        await message.answer("И тебе пока!")
+    else:
+        await message.answer("Я пока что не обучен отвечать на такие вопросы")
+
+
+async def main() -> None:
+    await dp.start_polling(bot)
+
+
+asyncio.run(main())
